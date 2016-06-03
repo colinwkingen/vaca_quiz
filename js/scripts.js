7f47523b52432164a33f75ede3580c43ed8308cf
Colin Kingen <colinwkingen@gmail.com>
2016-06-03 11:38:20 -0700

    Add a function to add CSS class to specific ID's based on survey result

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,56 @@
 var pointCrunch = function(totalPoints) {
-    var totalCrunched = 1;
+      var totalCrunched = " earth";
       if ( totalPoints <= 13 ) {
       alert(totalCrunched);
       return totalCrunched;
     } else if ( totalPoints <= 25 ) {
-      totalCrunched += 1;
+      totalCrunched = " mars";
       alert(totalCrunched);
       return totalCrunched;
     } else if ( totalPoints <= 34 ) {
-      totalCrunched += 2;
+      totalCrunched = " saturn";
       alert(totalCrunched);
       return totalCrunched;
     } else if ( totalPoints <= 42 ) {
-      totalCrunched += 3;
+      totalCrunched = " jupiter";
       alert(totalCrunched);
       return totalCrunched;
     } else if ( totalPoints <= 49 ) {
-      totalCrunched += 4;
+      totalCrunched = " venus";
       alert(totalCrunched);
       return totalCrunched;
     } else if ( totalPoints <= 57 ) {
-      totalCrunched += 5;
+      totalCrunched = " mercury";
       alert(totalCrunched);
       return totalCrunched;
     } else if ( totalPoints <= 66 ) {
-      totalCrunched += 6;
+      totalCrunched = " neptune";
       alert(totalCrunched);
       return totalCrunched;
     } else if ( totalPoints <= 78 ) {
-      totalCrunched += 7;
+      totalCrunched = " uranus";
       alert(totalCrunched);
       return totalCrunched;
     } else if ( totalPoints <= 91 ) {
-      totalCrunched += 8;
+      totalCrunched = " pluto";
       alert(totalCrunched);
       return totalCrunched;
     };
-};
+
+    var classPlanet = function(totalCrunched) {
+      var targetItem = document.getElementById("resultBox");
+      targetItem.className += totalCrunched;
+    };
+
+    *  1-Earth
+    *  2-Mars
+    *  3-Saturn (Moons)
+    *  4-Jupiter (Moons)
+    *  5-Venus
+    *  6-Mercury
+    *  7-Neptune
+    *  8-Uranus
+    *  9-Pluto
 
 // 13:12:9:8:7:8:9:12:13:
 // Distribution = these values as range.
@@ -56,5 +70,10 @@ $(document).ready(function() {
     pointCount += parseInt($("#questionTen").val());
     alert(pointCount);
     var result = pointCrunch(pointCount);
+    alert(result);
+
+
+
+
   });
 });
