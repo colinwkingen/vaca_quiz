8098a0a412072edb205dbfb08cb34b46090871f7
Colin Kingen <colinwkingen@gmail.com>
2016-06-03 13:52:48 -0700

    Create classes to hide form and show answer splash page.

--- js/scripts.js
+++ js/scripts.js
@@ -37,24 +37,6 @@ var pointCrunch = function(totalPoints) {
       return totalCrunched;
     };
 };
-var classPlanet = function(totalCrunched) {
-  var targetItem = document.getElementsByClassName("result_target");
-  targetItem.className += totalCrunched;
-};
-
-    // *  1-Earth
-    // *  2-Mars
-    // *  3-Saturn (Moons)
-    // *  4-Jupiter (Moons)
-    // *  5-Venus
-    // *  6-Mercury
-    // *  7-Neptune
-    // *  8-Uranus
-    // *  9-Pluto
-
-// 13:12:9:8:7:8:9:12:13:
-// Distribution = these values as range.
-//
 $(document).ready(function() {
   $("button#submitForm").click(function() {
     var pointCount = 0;
@@ -68,12 +50,11 @@ $(document).ready(function() {
     pointCount += parseInt($("#questionEight").val());
     pointCount += parseInt($("#questionNine").val());
     pointCount += parseInt($("#questionTen").val());
-    alert(pointCount);
+    // alert(pointCount);
     var result = pointCrunch(pointCount);
-    alert(result);
-    classPlanet(result);
-
-
-
+    // alert(result);
+    $('.result_target').addClass(result);
+    $('#theAnswer').text(result);
+    $('.initial').toggleClass('hidden');
   });
 });
